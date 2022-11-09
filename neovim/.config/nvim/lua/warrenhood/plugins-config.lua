9e57fefb03f9e149a16407508e085f0759b5583c
Warren Hood <warren.hood@hexagon.com>
2022-11-09 15:45:57 +0200

    Add treesitter config

--- neovim/.config/nvim/lua/warrenhood/plugins-config.lua
+++ neovim/.config/nvim/lua/warrenhood/plugins-config.lua
@@ -1,2 +1,24 @@
 -- nvim-tree setup with defaults
 require("nvim-tree").setup()
+
+-- tree-sitter config
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all"
+
+  ensure_installed = { "c", "lua", "rust", "bash", "python", "javascript" },
+
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+
+  highlight = {
+    enable = true,
+
+    additional_vim_regex_highlighting = false,
+  },
+}
