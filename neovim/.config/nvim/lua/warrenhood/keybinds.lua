36bea988a7a8d31391e7853484a1f86ac6363688
Warren Hood <nullbyte001@gmail.com>
2022-11-10 22:54:19 +0200

    Setup pane/window nav and ToggleTerm, and allow passing opts in map function

--- neovim/.config/nvim/lua/warrenhood/keybinds.lua
+++ neovim/.config/nvim/lua/warrenhood/keybinds.lua
@@ -1,5 +1,9 @@
-local function map(m, k, v)
-	vim.keymap.set(m, k, v, {silent = true})
+local function map(m, k, v, opts)
+    opts = opts or {}
+    if not opts['silent'] then
+        opts['silent'] = true
+    end
+	vim.keymap.set(m, k, v, opts)
 end
 
 
@@ -13,10 +17,25 @@ map('i', '<C-S>', '<ESC>:w<CR>')
 
 -- Telescope
 local telescope_builtin = require('telescope.builtin')
-map('n', '<leader>ff', telescope_builtin.find_files)
-map('n', '<leader>fg', telescope_builtin.live_grep)
-map('n', '<leader>fb', telescope_builtin.buffers)
-map('n', '<leader>fh', telescope_builtin.help_tags)
+map('n', '<leader>ff', telescope_builtin.find_files, { desc = 'Find Files' })
+map('n', '<leader>fg', telescope_builtin.live_grep, { desc =  'Live Grep'})
+map('n', '<leader>fb', telescope_builtin.buffers, { desc = 'Find Buffers'})
+map('n', '<leader>fh', telescope_builtin.help_tags, { desc = 'Help Tags' })
 
 -- NvimTree Toggle (File Explorer)
 map('n', '<C-B>', ':NvimTreeToggle<CR>')
+
+-- ToggleTerm
+map('n', '<C-t>', ':ToggleTerm<CR>')
+map('t', '<C-t>', '<C-\\><C-n>:ToggleTerm<CR>')
+
+-- Pane navigation
+map('n', '<C-h>', ':wincmd h<CR>')
+map('n', '<C-j>', ':wincmd j<CR>')
+map('n', '<C-k>', ':wincmd k<CR>')
+map('n', '<C-l>', ':wincmd l<CR>')
+
+map('t', '<C-h>', '<C-\\><C-n>:wincmd h<CR>')
+map('t', '<C-j>', '<C-\\><C-n>:wincmd j<CR>')
+map('t', '<C-k>', '<C-\\><C-n>:wincmd k<CR>')
+map('t', '<C-l>', '<C-\\><C-n>:wincmd l<CR>')
--- neovim/.config/nvim/lua/warrenhood/plugins-config.lua
+++ neovim/.config/nvim/lua/warrenhood/plugins-config.lua
@@ -22,3 +22,6 @@ require'nvim-treesitter.configs'.setup {
     additional_vim_regex_highlighting = false,
   },
 }
+
+
+
