f6c03e84a40b4ad974fcc0db986832f213681ad7
Warren Hood <nullbyte001@gmail.com>
2022-11-26 03:18:20 +0200

    Comment out titlebar code

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -513,9 +513,9 @@ awful.rules.rules = {
     }, properties = { floating = true } },
 
     -- Add titlebars to normal clients and dialogs
-    { rule_any = { type = { "normal", "dialog" }
-    }, properties = { titlebars_enabled = false }
-    },
+    -- { rule_any = { type = { "normal", "dialog" }
+    -- }, properties = { titlebars_enabled = false }
+    -- },
 
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
