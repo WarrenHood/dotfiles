2f509692f0a1d6f32f1030b53651e2233a97b3d6
Warren Hood <warren.hood@hexagon.com>
2023-01-25 11:05:29 +0200

    Remap toggleterm to Ctrl+T

--- neovim/.config/nvim/lua/warrenhood/keybinds.lua
+++ neovim/.config/nvim/lua/warrenhood/keybinds.lua
@@ -25,8 +25,8 @@ map('n', '<leader>fh', telescope_builtin.help_tags, { desc = 'Help Tags' })
 map('n', '<C-B>', ':NvimTreeToggle<CR>')
 
 -- ToggleTerm
-map('n', '<S-t>', ':ToggleTerm<CR>')
-map('t', '<S-t>', '<C-\\><C-n>:ToggleTerm<CR>')
+map('n', '<C-t>', ':ToggleTerm<CR>')
+map('t', '<C-t>', '<C-\\><C-n>:ToggleTerm<CR>')
 
 --[[ -- Pane navigation
 map('n', '<C-h>', ':wincmd h<CR>')
