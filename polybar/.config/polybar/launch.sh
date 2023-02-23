dcfd17952e972409722867c4d4d2a7c53a548505
Warren Hood <nullbyte001@gmail.com>
2023-02-23 06:22:08 +0200

    Used some icons for bspwm

---
+++ polybar/.config/polybar/launch.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+pkill polybar
+polybar --reload main &
+# if type "xrandr"; then
+#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#     MONITOR=$m polybar --reload main &
+#   done
+# else
+#   polybar --reload main &
+# fi
