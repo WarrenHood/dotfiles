91fe53d702baf2397b99c9fdabb685839eb02b62
Warren Hood <nullbyte001@gmail.com>
2023-04-12 20:22:49 +0200

    Added commands to open/close dapui as well cd to nvim config dir
    (cherry picked from commit e08702fd1d4f7362c28d50055c5f34eac34cf5b7)

--- neovim/.config/nvim/lua/warrenhood/commands.lua
+++ neovim/.config/nvim/lua/warrenhood/commands.lua
@@ -1,2 +1,11 @@
 -- Format file
 vim.cmd "silent! command! Format lua vim.lsp.buf.format()"
+
+-- Open dapui
+vim.cmd "silent! command! DapUIOpen lua require('dapui').open()"
+
+-- Close dapui
+vim.cmd "silent! command! DapUIOpen lua require('dapui').close()"
+
+-- Edit config
+vim.cmd "silent! command! ConfigOpen cd ~/.config/nvim"
