d01939f9bb78081c48746f687635bf2f69351b31
Warren Hood <nullbyte001@gmail.com>
2023-04-14 19:11:41 +0200

    Fix dapui open/close typo and add toggle command

--- neovim/.config/nvim/lua/warrenhood/commands.lua
+++ neovim/.config/nvim/lua/warrenhood/commands.lua
@@ -5,7 +5,10 @@ vim.cmd "silent! command! Format lua vim.lsp.buf.format()"
 vim.cmd "silent! command! DapUIOpen lua require('dapui').open()"
 
 -- Close dapui
-vim.cmd "silent! command! DapUIOpen lua require('dapui').close()"
+vim.cmd "silent! command! DapUIClose lua require('dapui').close()"
+
+-- Toggle dapui
+vim.cmd "silent! command! DapUIToggle lua require('dapui').toggle()"
 
 -- Edit config
 vim.cmd "silent! command! ConfigOpen cd ~/.config/nvim"
