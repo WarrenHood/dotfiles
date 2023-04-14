a8010ba139d1546b6afe1e51f52a2b776763f8bc
Warren Hood <warren.hood@hexagon.com>
2023-04-14 11:13:17 +0200

    fix typo

--- neovim/.config/nvim/lua/warrenhood/commands.lua
+++ neovim/.config/nvim/lua/warrenhood/commands.lua
@@ -5,7 +5,7 @@ vim.cmd "silent! command! Format lua vim.lsp.buf.format()"
 vim.cmd "silent! command! DapUIOpen lua require('dapui').open()"
 
 -- Close dapui
-vim.cmd "silent! command! DapUIOpen lua require('dapui').close()"
+vim.cmd "silent! command! DapUIClose lua require('dapui').close()"
 
 -- Edit config
 vim.cmd "silent! command! ConfigOpen cd ~/.config/nvim"
