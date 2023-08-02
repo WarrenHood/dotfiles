fd3932e331d4d77f2fb00dec0daf14fbdf1d8850
Warren Hood <nullbyte001@gmail.com>
2023-08-03 00:42:09 +0200

    Set dpi to 96

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -619,6 +619,7 @@ awful.spawn.once("picom") -- Compositor
 awful.spawn.once("nm-applet") -- Network Manager applet
 awful.spawn.once("autorandr -l dual") -- Load my dual monitor autorandr config
 awful.spawn.once("volumeicon") -- Volume icon
+awful.spawn.once("xrandr --dpi 96") -- Set dpi to 96
 
 -- Wallpaper
 -- awful.spawn.once("xargs xwallpaper --output all --stretch --center ~/.config/awesome/wallpapers/miku_1.png")
